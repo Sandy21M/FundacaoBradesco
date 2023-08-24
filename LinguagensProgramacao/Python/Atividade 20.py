1480c8e8c9671dc83ea3e6207ecd0c02508b492c
Sandryane Maria <142358771+Sandy21M@users.noreply.github.com>
2023-08-24 23:02:09 +0000

    alterações no exercicio 20

--- LinguagensProgramacao/Python/Atividade 20.py
+++ LinguagensProgramacao/Python/Atividade 20.py
@@ -1,4 +1,21 @@
-operacao=0
-def calculadora operacao(num1, num2, operacao)
-operacao
-
+operacao = int(input('Digite um número: 1-Para soma \n 2-Para subração\n 3-Para subtração\n 4-Para divisão\n 0-Para sair'))
+ 
+while operacao !=0:
+    if operacao >=0 and operacao <4:
+        num1 = float(input('Digite um número:'))
+        num2 = float(input('Digite um número:'))
+        if operacao==1:
+            resultado =  num1 + num2
+        elif operacao == 2: 
+            resultado = num1 - num2
+        elif operacao == 3:
+            resultado = num1 * num2
+        elif operacao == 4:
+            resultado = num1 / num2
+        print ('O resultado é:', resultado )    
+        operacao = int(input('Digite um número: 1-Para soma \n 2-Para subração\n 3-Para subtração\n 4-Para divisão \n 0-Para sair'))
+    else:
+        print('Operação invalida.')
+        operacao = int(input('Digite um número: 1-Para soma \n 2-Para subração\n 3-Para subtração\n 4-Para divisão\n 0-Para sair '))
+print('Opção de saída realizada com sucesso.')
+             
