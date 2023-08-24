43bda6c1f26a6aefb5ced849552f37fd3c150177
Sandryane Maria <142358771+Sandy21M@users.noreply.github.com>
2023-08-24 23:38:14 +0000

    segundas alterações no signos

--- LinguagensProgramacao/Python/signos.py
+++ LinguagensProgramacao/Python/signos.py
@@ -1,1 +1,5 @@
-signos = []
+
+signos = ['Macaco', 'Gaio', 'Cão', 'Porco', 'Rato', 'Boi', 'Tigre', 'Coelho', 'DragãO', 'Serpente', 'Cavalo', 'Carneiro']
+ano = int(input('Digite o ano em que nasceu:'))
+indice = ano % 12
+print(signos[indice])
