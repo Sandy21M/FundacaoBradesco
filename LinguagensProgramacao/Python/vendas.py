ed47140411208c534fdd49ee38ae0841c3e43991
Sandryane Maria <142358771+Sandy21M@users.noreply.github.com>
2023-08-25 00:08:37 +0000

    segundas aterações na atividade d vendas

---
+++ LinguagensProgramacao/Python/vendas.py
@@ -0,0 +1,9 @@
+venda=[]
+i = 0
+while i <30:
+    produto = input('Informe o produto que deseja:')
+    quantidade = int(input('Informe a quantidade que deseja:'))
+    venda[produto] = quantidade
+i = i + 1
+
+
